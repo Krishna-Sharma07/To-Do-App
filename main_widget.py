64dba8110a7968e2201bf1e7f1b8a7711e75f7e8
Krishna Sharma <krishnasharma.dli7@gmail.com>
2025-07-31 17:54:56 +0530

    Figured out how to resize QListWidget

--- main_widget.py
+++ main_widget.py
@@ -5,16 +5,17 @@ class Main_Widget(QWidget):
         super().__init__()
         task_list_lable = QLabel("Task Lists")
         list_of_lists = QListWidget()
-        list_of_lists.resize(300, 1000)
-
+        list_of_lists.setMinimumSize(300, 1000)
+        list_of_lists.setMaximumSize(700, 2000)
         task_list_layout = QVBoxLayout()
         task_list_layout.addWidget(task_list_lable)
         task_list_layout.addWidget(list_of_lists)
 
 
-        tasks_widget = QListWidget()
-        tasks_widget.resize(700, 1000)
 
+        tasks_widget = QListWidget()
+        tasks_widget.setMinimumSize(700, 1000)
+        tasks_widget.setMaximumSize(2000, 2000)
         layout = QHBoxLayout()
         layout.addLayout(task_list_layout)
         layout.addWidget(tasks_widget)
